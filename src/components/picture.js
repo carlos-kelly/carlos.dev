f1c822d8b169e73b89440b8918536178d73bf99f
Carlos Paelinck <carlos@carlospaelinck.com>
2019-02-18 16:33:47 -0600

    Remove old code

--- src/components/picture.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Picture = styled.img`
-  width: 150px;
-  height: 150px;
-  border-radius: 75px;
-  border: 2px solid #263238;
-  margin: 0 0 1em;
-`;
--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const appFont =
-  '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Helvetica Neue", Helvetica, sans-serif';
-export const color = "#2a2626";
